80f273e9d18ed91373ddd107a067535ea80d4233
tmgriffin <>
2021-07-26 17:44:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+# Creating Turtle and Screen
+t: Turtle = Turtle()
+s: Screen = Screen()
+
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
