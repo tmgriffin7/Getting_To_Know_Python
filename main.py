c13be2d77a3dee9313075f0c3715dc93e96e978d
Tracy Griffin <87707883+tmgriffin7@users.noreply.github.com>
2021-07-26 18:24:42 +0000

    Added sideLength and input box

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from turtle import Turtle, Screen
 t: Turtle = Turtle()
 s: Screen = Screen()
 
+#sideLength: int = int (input('Give me a side length: '))
+sideLength: int = s.numinput('Input Window', 'Give me a side length:')
+
 # Draw square
-t.forward(150)
+t.forward(sideLength)
 t.right(90)
-t.forward(150)
+t.forward(sideLength)
 t.right(90)
-t.forward(150)
+t.forward(sideLength)
 t.right(90)
-t.forward(150)
+t.forward(sideLength)
 t.right(90)
